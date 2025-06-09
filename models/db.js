6fbb68e64ed7e1a58c2439f36c56a4aca395c427
asrien <ehdqls8387@naver.com>
2025-06-09 15:36:16 +0900

    modify model

--- models/db.js
+++ models/db.js
@@ -1,17 +1,21 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-module.exports = pool.promise();
+db.connect((err) => {
+  if (err) {
+    console.error("DB 연결 실패:", err);
+  } else {
+    console.log("DB 연결 성공");
+  }
+});
+
+module.exports = db;
